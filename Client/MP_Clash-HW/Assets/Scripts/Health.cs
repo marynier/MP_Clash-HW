0f7678821ad1f57a63199b2d08900e3bb449e389
marynier <mafukljaa@gmail.com>
2025-11-29 18:26:06 +0400

    HealthUI

--- Client/MP_Clash-HW/Assets/Scripts/Health.cs
+++ Client/MP_Clash-HW/Assets/Scripts/Health.cs
@@ -3,11 +3,13 @@
 public class Health : MonoBehaviour
 {
     [field: SerializeField] public float max { get; private set; } = 10f;
+    [SerializeField] private HealthUI _ui;
     private float _current;
 
     private void Start()
     {
         _current = max;
+        _ui.UpdateHealth(max, _current);
     }
 
     public void ApplyDamage(float value)
@@ -15,7 +17,14 @@ public class Health : MonoBehaviour
         _current -= value;
         if (_current < 0) _current = 0;
 
+        UpdateHP();
+
         Debug.Log($"Объект {name}: было {_current + value}, стало {_current}");
+    }
+
+    private void UpdateHP()
+    {
+        _ui.UpdateHealth(max, _current);
     }
 }
 
---
+++ Client/MP_Clash-HW/Assets/Scripts/HealthUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    [SerializeField] private Image _filledImage;
+
+    public void UpdateHealth(float max, float current)
+    {
+        Color color;
+        float percent = current / max;
+
+        if (percent > 0.7f) color = Color.green;
+        else if (percent > 0.4f) color = Color.yellow;
+        else color = Color.red;
+
+        _filledImage.fillAmount = percent;
+        _filledImage.color = color;
+    }
+}
