ef13fb34e5bd5d70c4f1c60c9ced652922748e42
marynier <mafukljaa@gmail.com>
2025-12-28 19:22:57 +0400

    Server

---
+++ Server/MP_Clash_HW/src/Library.ts
@@ -0,0 +1,4 @@
+export namespace Library{
+    export const phpKEY = 'ggddntnvtgh!@5767gjg,%%#';
+    export const getDeckURI = 'http://localhost/Projects/LessonDatabase/Colyseus/getDeck.php';
+}
---
+++ Server/MP_Clash_HW/src/rooms/02-state-handler.ts
@@ -0,0 +1,76 @@
+import { Room, Client, Delayed } from "colyseus";
+import { Schema, type, MapSchema } from "@colyseus/schema";
+import axios from 'axios';
+import { Library } from "../Library";
+
+export class Player extends Schema {   
+}
+
+export class State extends Schema {
+    @type({ map: Player })
+    players = new MapSchema<Player>();
+
+    createPlayer(sessionId: string) {
+        this.players.set(sessionId, new Player());
+    }
+
+    removePlayer(sessionId: string) {
+        this.players.delete(sessionId);
+    }    
+}
+
+export class StateHandlerRoom extends Room<State> {
+    maxClients = 2;
+    playersDeck = new Map();
+
+    onCreate (options) {
+        console.log("StateHandlerRoom created!", options);
+
+        this.setState(new State());
+
+        this.onMessage("move", (client, data) => {
+            console.log("StateHandlerRoom received message from", client.sessionId, ":", data);
+            //this.state.movePlayer(client.sessionId, data);
+        });
+    }
+    
+    gameIsStarted: boolean = false;
+    awaitStart: Delayed;
+    async onJoin (client: Client, data) {
+        try {
+            const response = await axios.post(Library.getDeckURI, {key: Library.phpKEY, userID: data.id});
+            console.log(response.data);
+            this.playersDeck.set(client.id, response.data);
+        } catch (error) {
+            console.log('Вылетела ошибка ' + error);
+        }
+        this.state.createPlayer(client.sessionId);
+
+        if(this.clients.length < 2) return;
+
+        this.broadcast("GetReady");
+        this.awaitStart = this.clock.setTimeout(() => {
+            try {
+                this.broadcast("Start", JSON.stringify({player1ID: this.clients[0].id, player1: this.playersDeck.get(this.clients[0].id), player2: this.playersDeck.get(this.clients[1].id)}));
+                this.gameIsStarted = true;
+            } catch (error) {
+                this.broadcast("CancelStart");
+            }                     
+        }, 1000);
+    }
+
+    onLeave (client) {
+        if(this.gameIsStarted == false && this.awaitStart !== undefined && this.awaitStart.active){
+            this.broadcast("CancelStart");
+            this.awaitStart.clear();
+        }  
+
+        if(this.playersDeck.has(client.if))
+            this.playersDeck.delete(client.id);
+        this.state.removePlayer(client.sessionId);
+    }
+
+    onDispose () {
+    }
+
+}
